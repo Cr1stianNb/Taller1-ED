8817844184f24d652a59e8abbe237815d1e586d0
Cr1stianNb <130185151+Cr1stianNb@users.noreply.github.com>
2023-10-06 16:29:49 +0000

    Documentando

--- Logica/VisitorUsuario.h
+++ Logica/VisitorUsuario.h
@@ -3,22 +3,46 @@
 #include "IVisitorUsuario.h"
 
 
-
+// Forward declaration
 class Administrador;
 class UsuarioMenor;
 class UsuarioMayor;
 
-
+/**
+ * @class VisitorUsuario
+*/
 class VisitorUsuario : public IVisitor
 {
     private:
         int tipoUsuario;
     public:
+        /**
+         * Constructor de la Clase VisitorUsuario
+        */
         VisitorUsuario();
+        /**
+         * Destructor
+        */
         ~VisitorUsuario();
+        /**
+         * Métodos override de la 'Interfaz IVisitor'
+         * @param Administrador objeto administrador a visitar
+        */
         void acepta(Administrador);
+         /**
+         * Métodos override de la 'Interfaz IVisitor'
+         * @param UsuarioMenor objeto UsuarioMenor a visitar
+        */
         void acepta(UsuarioMenor);
+         /**
+         * Métodos override de la 'Interfaz IVisitor'
+         * @param UsuarioMayor objeto UsuarioMayor a visitar
+        */
         void acepta(UsuarioMayor);
+        /**
+         * Tipo de usuario que visito
+         * @return un entero que define que tipo de usuario visito
+        */
         int getTipo();
 };
 
